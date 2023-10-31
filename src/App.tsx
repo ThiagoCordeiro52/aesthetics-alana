617257b5a8a10b1a53fa74f6e9c5575d3fbcdb7f
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2023-10-31 19:33:27 -0300

    feat: changed the avatar image

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import styles from "./App.module.css";
-import "./global.css";
+import styles from './App.module.css';
+import './global.css';
 
-import { Avatar } from "./components/Avatar";
-import { LinksList } from "./components/LinksList";
-import { Footer } from "./components/Footer";
+import { Avatar } from './components/Avatar';
+import { LinksList } from './components/LinksList';
+import { Footer } from './components/Footer';
 
-import DarkThemeAvatar from "/src/assets/avatar.jpg";
-import LightThemeAvatar from "/src/assets/avatar-light.jpg";
+import DarkThemeAvatar from '/src/assets/avatar.jpg';
+import LightThemeAvatar from '/src/assets/avatar-light.jpg';
 
 function App() {
-  const [theme, setTheme] = useState<"dark" | "light">("dark");
+  const [theme, setTheme] = useState<'dark' | 'light'>('dark');
 
   function toggleTheme() {
-    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
   }
 
   function currentAvatar() {
-    return theme === "dark" ? DarkThemeAvatar : LightThemeAvatar;
+    return theme === 'dark' ? DarkThemeAvatar : LightThemeAvatar;
   }
 
   return (
     <div
-      className={`${styles.container} ${theme === "light" ? styles.light : ""}`}
+      className={`${styles.container} ${theme === 'light' ? styles.light : ''}`}
     >
       <Avatar filepath={currentAvatar()} />
 
